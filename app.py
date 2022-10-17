781b5d0c388195ff52e2367165cafa6768d585ac
nob11s <108393398+nob11s@users.noreply.github.com>
2022-10-17 13:42:52 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import requests,os,sys
+
+def logo () :
+    print("""""
+░██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗
+░██║░░██╗░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗████╗░████║██╔════╝
+░╚██╗████╗██╔╝█████╗░░██║░░░░░██║░░╚═╝██║░░██║██╔████╔██║█████╗░░
+░░████╔═████║░██╔══╝░░██║░░░░░██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░
+░░╚██╔╝░╚██╔╝░███████╗███████╗╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗
+░░░╚═╝░░░╚═╝░░╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝""");
+
+
+def menu() :
+    os.system('clear');
+    logo();
+    print("+----------------+");
+    print("  TOOLS TERMUX");
+    print("+----------------+");
+    print("[1] Install Tombol Termux");
+    print("[2] Keluar Menu");
+    nmr = input("Pilih Sesuai Nomor :");
+
+    # Setting Bagian Tools 
+    if nmr =="1":
+        os.system("pkg update && pkg upgrade");
+        os.system("pkg install python2 -y");
+        os.system("pkg install git -y");
+        os.system("git clone https://github.com/kumpulanremaja/termuxtbb");
+
+    elif nmr =="2":
+        sys.exit();
+
+menu();
